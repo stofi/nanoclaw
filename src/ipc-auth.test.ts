546807d000bf052bc4f6164ac9b86fc597d94216
Kryštof Rainisch <krystof.rainisch@hey.com>
2026-02-28 22:38:05 +0100

    fix: add pushTasksSnapshotsAfterChange stub to test deps

--- src/ipc-auth.test.ts
+++ src/ipc-auth.test.ts
@@ -61,6 +61,7 @@ beforeEach(() => {
     syncGroupMetadata: async () => {},
     getAvailableGroups: () => [],
     writeGroupsSnapshot: () => {},
+    pushTasksSnapshotsAfterChange: () => {},
   };
 });
 
